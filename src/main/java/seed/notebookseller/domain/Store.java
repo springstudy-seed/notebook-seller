5b3a56a4720c208b6778806981d5702b60fdbbac
kssumin <201566@jnu.ac.kr>
2022-11-23 18:41:11 +0900

    chore(Store) : 접근 제어자 변경

--- src/main/java/seed/notebookseller/domain/Store.java
+++ src/main/java/seed/notebookseller/domain/Store.java
@@ -1,18 +1,22 @@
 package seed.notebookseller.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 @NoArgsConstructor
+@Getter
 public class Store {
     @Id
-    public Long id;
-    public String location;
-    public String name;
+    private Long id;
+    private String location;
+    private String name;
+
 
     public Store(String location, String name) {
         this.location = location;
