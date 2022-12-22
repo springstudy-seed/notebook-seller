48b4a897dc0cdc2c46ad1db42b3c2fe8570187ca
kssumin <201566@jnu.ac.kr>
2022-12-22 17:34:40 +0900

    feat(DepositController) : createDeposit location header 추가

--- src/main/java/seed/notebookseller/controller/DepositController.java
+++ src/main/java/seed/notebookseller/controller/DepositController.java
@@ -1,8 +1,12 @@
 package seed.notebookseller.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import seed.notebookseller.domain.Deposit;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,9 +14,14 @@ public class DepositController {
     private final DepositService depositService;
 
     @PostMapping("/api/deposits")
-    public boolean createDeposit(@RequestBody DepositDto depositDto){
-        depositService.createDeposit(depositDto);
-        return true;
+    public ResponseEntity<Deposit> createDeposit(@RequestBody DepositDto depositDto){
+        Deposit createdDeposit=depositService.createDeposit(depositDto);
+        URI uri=ServletUriComponentsBuilder.fromCurrentRequest()
+                        .path("{id}/")
+                        .buildAndExpand(createdDeposit.getId())
+                        .toUri();
+        return ResponseEntity.created(uri)
+                .body(createdDeposit);
     }
 
     @PatchMapping("/api/deposits/{id}")
